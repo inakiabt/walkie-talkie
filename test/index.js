44f39f1f684d79238fd62aed2065e6236d1df253
Brandon Brown <brandon@brandonb.io>
2014-07-13 22:04:38 -0700

    Add mocha tests

--- test/index.js
+++ test/index.js
@@ -1,44 +1,56 @@
-var settings = {
-  "API_KEY": "your_API_key",
-  "APP_ID": "your_APP_ID"
-}
+var intercom = require("../intercom").app(settings),
+    should = require('chai').should(),
+    settings = {
+      "APP_ID": "",
+      "API_KEY": ""
+    };
 
-var intercom = require("../intercom").app(settings);
+describe('Intercom users', function() {
+  // it('DESCRIPTION', function() {
+  //   // blah().should.equal('bar');
+  // });
 
-intercom.users.all(function(code, body){
-  console.log(code, body.toString());
-});
+  it('returns all users', function() {
+    intercom.users.all(function(code, body) {
+      console.log(code, body.toString());
+    });
+  });
 
-intercom.users.get("brandon@brandonb.io", function(code, body){
-  console.log(code, body.toString());
-});
 
-var data = {
-  "email" : "jorge@chloi.io",
-  "name" : "Jorge Predet",
-  "created_at" : (new Date() / 1000),
-  "pre_launch" : true,
-  "last_seen_ip" : "1.2.3.4",
-  "last_seen_user_agent" : "ie6"
-}
+  it('returns data for a single user by email', function() {
+    intercom.users.get("brandon@brandonb.io", function(code, body) {
+      console.log(code, body.toString());
+    });
+  });
 
-intercom.users.post(data, function(code, body){
-  console.log(code, body.toString());
-});
+  it('creates a user and return a response', function() {
+    intercom.users.post({
+      "email" : "test@brandonb.io",
+      "name" : "Test McTesterson",
+      "created_at" : (new Date() / 1000),
+      "pre_launch" : true,
+      "last_seen_ip" : "1.2.3.4",
+      "last_seen_user_agent" : "ie6"
+    }, function(code, body) {
+      console.log(code, body.toString());
+    });
+  });
 
-var data = {
-  "email" : "jorge@chloi.io",
-  "name" : "Jorgie"
-}
+  it('updates a user and returns a response', function() {
+    intercom.users.put({
+      "email" : "test@brandonb.io",
+      "name" : "Jorgie"
+    }, function(code, body) {
+      console.log(code, body.toString());
+    });
+  });
 
-intercom.users.put(data, function(code, body){
-  console.log(code, body.toString());
-});
+  it('deletes a user by email and returns a response', function() {
+    intercom.users.delete({
+      "email": "test@brandonb.io"
+    }, function(code, body) {
+      console.log(code, body.toString());
+    });
+  });
 
-var data = {
-  "email": "jorge@chloi.io"
-}
-
-intercom.users.delete(data, function(code, body){
-  console.log(code, body.toString());
 });
