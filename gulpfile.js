e5c8bcfbb783b468c52a4515becd2c2a73844357
Brandon Brown <brandon@brandonb.io>
2014-07-15 15:16:06 -0700

    Remove gulp-git, causing issues

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,4 @@
 var gulp        = require('gulp');
-var git         = require('gulp-git');
 var mocha       = require('gulp-mocha');
 var jshint      = require('gulp-jshint');
 var stylish     = require('jshint-stylish');
@@ -19,21 +18,9 @@ gulp.task('mocha', ['lint'],  function() {
     .pipe(mocha({ reporter: 'spec' }));
 });
 
-gulp.task('tag', function() {
-  var pkg     = require('./package.json');
-  var v       = 'v' + pkg.version;
-  var message = 'Release ' + v;
-
-  gulp.src('./').pipe(git.commit(message));
-  git.tag(v, message);
-  return git.push('origin', 'master', { args: ' --tags' }).end();
-});
-
 gulp.task('npm', ['tag'], function(done) {
   var spawn = require('child_process').spawn;
   spawn('npm', ['publish'], { stdio: 'inherit' }).on('close', done);
 });
 
 gulp.task('test', ['mocha']);
-
-gulp.task('release', ['npm']);
